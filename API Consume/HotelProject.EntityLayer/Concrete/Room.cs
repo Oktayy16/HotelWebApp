578c87dc13050c321769e455e5842e8fcfd9656a
Oktayy16 <oktay16arslan@hotmail.com>
2023-08-26 00:31:21 +0300

    first commit

---
+++ API Consume/HotelProject.EntityLayer/Concrete/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public string RoomNumber { get; set; } // Odalar sadece rakam değil metinsel değerde alacak 5C gibi
+        public string RoomCoverImage { get; set; } // resim
+        public int Price { get; set; }
+        public string Title { get; set; }
+        public string BenCount { get; set; }
+        public string BathCount { get; set; }
+        public string Wife { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
