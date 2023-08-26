62e432b7aea427b84a0568eb494765ca514d1ff1
Oktayy16 <oktay16arslan@hotmail.com>
2023-08-26 11:55:27 +0300

    ikinci gün

---
+++ API Consume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,27 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+
+        // Bağlantı adresimizi yazıyoruz
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-N2KFL21;initial catalog=ApiDb;integrated security=true;TrustServerCertificate=True;"); // TrustServerCertificate=True; bu eksik olduğu için yazdıramamışım
+        }
+
+        // Tablolar
+        public DbSet<Room> Rooms { get; set; } // Room sınıf ismi, Rooms sql e yansıyacak tablo ismi
+        public DbSet<Service> Services { get; set; } 
+        public DbSet<Subscribe> Subscribes { get; set; } 
+        public DbSet<Testimonial> Testimonials { get; set; } 
+    }
+}
